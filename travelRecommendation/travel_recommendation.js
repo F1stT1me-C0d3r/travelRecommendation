a0cff6dee7dfc4ba2a26309cd068b9294e4d3e8d
F1stT1me-C0d3r <mckenxzy.boa@gmail.com>
2026-01-30 14:16:40 -0500

    working on key workd cards

--- travelRecommendation/travel_recommendation.js
+++ travelRecommendation/travel_recommendation.js
@@ -3,10 +3,5 @@
 
 fetch('travel_recommendation_api.json')
  .then(response => response.json)
- .then(data => {})
+ .then(data => console.log(data))
  .catch(err => console.error(err));
-
-var searchbtn=document.getElementById("search").innerHTML;
-searchbtn.onclick=function(){
-    alert("button clicked")
-};
