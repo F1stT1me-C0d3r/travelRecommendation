e7ecfc86a523929542ff60e8653efffe88aa7034
F1stT1me-C0d3r <mckenxzy.boa@gmail>
2026-02-05 16:22:37 -0500

    need to combine search funtion with fetch API data specifically for template id

--- travelRecommendation/travel_recommendation.js
+++ travelRecommendation/travel_recommendation.js
@@ -1,12 +1,9 @@
-const userCardTemplate = document.querySelector("#data-user-template");
-console.log(userCardTemplate)
-
-const userCardContainer = document.querySelector("[data-user-cards-container]");
+const search = document.getElementById('search');
+const clear = document.getElementById('clear');
 
 fetch('./travel_recommendation_api.json')
  .then(res => res.json())
- .then(data => {  
-    const card = userCardTemplate.cloneNode(true).textContent
-    console.log(card)    
-});
+ .then(data => {
+    
+ });
 
