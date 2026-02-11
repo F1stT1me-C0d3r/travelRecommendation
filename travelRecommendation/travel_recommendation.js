a204279251bbb1b50755a92174e0799a9ff3cc2a
F1stT1me-C0d3r <mckenxzy.boa@gmail.com>
2026-02-11 16:46:03 -0500

    just gonna run javascript through AI to see wher I went wrong with querySelector

--- travelRecommendation/travel_recommendation.js
+++ travelRecommendation/travel_recommendation.js
@@ -1,9 +1,16 @@
-const search = document.getElementById('search');
-const clear = document.getElementById('clear');
+const locationSearch = document.querySelector('#locationSearch');
+/*const searchButton = document.getElementById('searchButton');
+const clear = document.getElementById('clearButton');*/
 
-fetch('./travel_recommendation_api.json')
+
+locationSearch.addEventListener('keyup', e => {
+    let currentValue = e.target.value.toLowerCase();
+    console.log(currentValue);
+});
+
+
+/*fetch('./travel_recommendation_api.json')
  .then(res => res.json())
  .then(data => {
-    
- });
+}); Uncomment once I've figured out the full extent of the fetchAPI */
 
